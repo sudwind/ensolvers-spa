3933aedd443abb0067aefda890f4804e2a69d991
sudwind <1626191+sudwind@users.noreply.github.com>
2022-01-29 16:49:49 -0300

    honshu: add CORS configuration

--- backend/src/main/java/de/paulsen/honshu/HonshuApplication.java
+++ backend/src/main/java/de/paulsen/honshu/HonshuApplication.java
@@ -1,13 +1,35 @@
 package de.paulsen.honshu;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class HonshuApplication {
+
+	private final Logger logger = LoggerFactory.getLogger(HonshuApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HonshuApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("Access-Control-Allow-Origin", "*")
+						.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+				logger.info("CORS Configuration set");
+			}
+		};
+	}
 }
