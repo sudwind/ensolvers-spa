0d01aa4a56729a987521b0a5d9824df3af317463
sudwind <1626191+sudwind@users.noreply.github.com>
2022-01-29 16:49:47 -0300

    honshu: add Item entity class

---
+++ backend/src/main/java/de/paulsen/honshu/item/Item.java
@@ -0,0 +1,64 @@
+package de.paulsen.honshu.item;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+public class Item {
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+
+	private String task;
+	private boolean isCompleted;
+
+	public Item() {}
+
+	public Item(String id, String task, boolean isCompleted) {
+		this.id = id;
+		this.task = task;
+		this.isCompleted = isCompleted;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void setCompleted(boolean completed) {
+		isCompleted = completed;
+	}
+
+	@Override
+	public String toString() {
+		return "Item{" +
+				"id='" + id + '\'' +
+				", task='" + task + '\'' +
+				", isCompleted=" + isCompleted +
+				'}';
+	}
+}
