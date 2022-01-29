4728e0345ad217558d22f1bbc1bbd4e2756fad8d
sudwind <1626191+sudwind@users.noreply.github.com>
2022-01-29 16:49:47 -0300

    initial bringup

---
+++ backend/src/main/java/de/paulsen/honshu/HonshuApplication.java
@@ -0,0 +1,13 @@
+package de.paulsen.honshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonshuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonshuApplication.class, args);
+	}
+
+}
