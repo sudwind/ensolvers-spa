092fdf2317fc1f2d6c77392e730461e7c258dd69
sudwind <1626191+sudwind@users.noreply.github.com>
2022-01-29 16:49:50 -0300

    honshu: remove unused imports

--- backend/src/main/java/de/paulsen/honshu/HonshuApplication.java
+++ backend/src/main/java/de/paulsen/honshu/HonshuApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
